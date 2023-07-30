49bb0917a9be6d0842a084f4aefef124d803d482
Nickolay <xenothorn@yandex.ru>
2023-07-30 11:41:27 +0300

    complete

--- main.cpp
+++ main.cpp
@@ -1,20 +1,128 @@
-#include "simple_vector.h"
+#include <cassert>
 #include <iostream>
-// Tests
-#include "tests.h"
-#include <string>
-
+#include <numeric>
 #include <vector>
+#include <deque>
 
 using namespace std;
-int main() {
-    Test1();
-    Test2();
+/*
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+    deque<typename RandomIt::value_type> pool;
 
-    TestReserveConstructor();
-    TestReserveMethod();
+    for (auto it = first; it!=last; ++it)
+    {
+        pool.push_back(move(*it));
+    }
+    size_t cur_pos = 0;
 
+    cout << "cp : " << cur_pos << "   ss : " << step_size << "    ps : " << pool.size() << endl;
+
+    while (!pool.empty()) {
+        *(first++) = pool[cur_pos];
+        pool.erase(pool.begin() + cur_pos);
+        cout << cur_pos << " ";// endl;
+        if (pool.empty()) {
+            cout << endl << "empty" << endl;
+            break;
+        }
+        cur_pos = (cur_pos + step_size - 1) % pool.size();
+
+        cout << cur_pos << endl;
+    }
+
+    auto pit = pool.begin();
+
+   
+
+}*/
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+    deque<typename RandomIt::value_type> pool; //(first, last);
+    for(auto it = first; it != last; ++it) {
+        pool.push_back(move(*it));
+    }
+ 
+    size_t cur_pos = 0;
+    auto it = first;
+    while (!pool.empty()) {
+        
+        move(pool.begin() + cur_pos, pool.begin() + cur_pos + 1, it++);
+        
+        pool.erase(pool.begin() + cur_pos);
+        if (pool.empty()) {
+            break;
+        }
+        cur_pos = (cur_pos + step_size - 1) % pool.size();
+    }
+}
+
+
+vector<int> MakeTestVector() {
+    vector<int> numbers(10);
+    iota(begin(numbers), end(numbers), 0);
+    return numbers;
+}
+
+void TestIntVector() {
+    const vector<int> numbers = MakeTestVector();
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+        assert(numbers_copy == numbers);
+    }
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+        assert(numbers_copy == vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
+    }
+}
+
+// Это специальный тип, который поможет вам убедиться, что ваша реализация
+// функции MakeJosephusPermutation не выполняет копирование объектов.
+// Сейчас вы, возможно, не понимаете как он устроен, однако мы расскажем
+// об этом далее в нашем курсе
+
+struct NoncopyableInt {
+    int value;
+
+    NoncopyableInt(const NoncopyableInt&) = delete;
+    NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+
+    NoncopyableInt(NoncopyableInt&&) = default;
+    NoncopyableInt& operator=(NoncopyableInt&&) = default;
+};
+
+bool operator==(const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
+    return lhs.value == rhs.value;
+}
+
+ostream& operator<<(ostream& os, const NoncopyableInt& v) {
+    return os << v.value;
+}
+
+void TestAvoidsCopying() {
     
-    std::cout << " -- OK --- " << std::endl;
-    return 0;
+    vector<NoncopyableInt> numbers;
+    numbers.push_back({1});
+    numbers.push_back({2});
+    numbers.push_back({3});
+    numbers.push_back({4});
+    numbers.push_back({5});
+
+    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+
+    vector<NoncopyableInt> expected;
+    expected.push_back({1});
+    expected.push_back({3});
+    expected.push_back({5});
+    expected.push_back({4});
+    expected.push_back({2});
+
+    assert(numbers == expected);
+}
+
+int main() {
+    TestIntVector();
+    TestAvoidsCopying();
 }
